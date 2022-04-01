80f7bfe38be5cc920d795d943f77003a22601305
Naitik10march <79587004+Naitik10march@users.noreply.github.com>
2022-04-01 19:40:30 +0530

    Add files via upload

---
+++ yash.py
@@ -0,0 +1,9 @@
+intoString = "my name is willy, i am 15 years old"
+words = intoString.split()
+print(words)
+words = intoString.split(",")
+print(words)
+
+def greet(name):
+ print("Hello, " + name + " . how are you?")
+greet("naitik")
